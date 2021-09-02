3b5e8a21c438c2f15124d36f4a1ab0179331cb30
AhmadSalihu <Ahmadsalihu419@gmail.com>
2021-09-02 11:38:04 +0100

    uploding staff images

--- src/Pages/ngsoft.staff.js
+++ src/Pages/ngsoft.staff.js
@@ -1,23 +1,54 @@
 import React from 'react';
 import { FaFacebookF, FaTwitter, FaInstagram } from 'react-icons/fa';
 
-import Member1 from '../assets/team/member-1.png';
-import Member2 from '../assets/team/member-2.png';
-import Member3 from '../assets/team/member-3.png';
-import Member4 from '../assets/team/member-4.png';
-import Member5 from '../assets/team/member-5.png';
-import Member6 from '../assets/team/member-6.png';
+import Kabirutafida from '../assets/NGSoft-staff-images/kabiru-tafida.jpg';
+import Salimtafida from '../assets/NGSoft-staff-images/salim-Tafida.jpg';
+import Hauwahayat from '../assets/NGSoft-staff-images/hauwa-hayat.jpg';
+import Davidruwan from '../assets/NGSoft-staff-images/david-ruwan.jpg';
+import Davidjames from '../assets/NGSoft-staff-images/david-james.jpg';
+import Murnajohn from '../assets/NGSoft-staff-images/murna-john.jpg';
+import AminaIbrahim from '../assets/NGSoft-staff-images/amina-Ib.jpg';
 
 import TeamCard from './team-card';
 
+
+
 import "../components/home.css"
 
+
 const data = [
-  {
+  // {
+  //   id: 1,
+  //   imgSrc: Member1,
+  //   altText: 'Olusegun Johnson Awonuga',
+  //   title: 'Olusegun Johnson Awonuga',
+  //   designation: 'Management Team',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
+   {
     id: 1,
-    imgSrc: Member1,
-    altText: 'Olusegun Johnson Awonuga',
-    title: 'Olusegun Johnson Awonuga',
+    imgSrc: Davidjames,
+    altText: 'David James',
+    title: 'David James',
     designation: 'Management Team',
     socialProfile: [
       {
@@ -40,12 +71,12 @@ const data = [
       },
     ],
   },
-  {
-    id: 2,
-    imgSrc: Member2,
-    altText: 'Hajiya Maimuna Ibrahim',
-    title: 'Hajiya Maimuna Ibrahim',
-    designation: 'Managemnt Team',
+   {
+    id: 1,
+    imgSrc: Davidruwan,
+    altText: 'Miss David Ruwan',
+    title: 'David Ruwan',
+    designation: 'Management Team',
     socialProfile: [
       {
         id: 1,
@@ -67,36 +98,63 @@ const data = [
       },
     ],
   },
-  {
-    id: 3,
-    imgSrc: Member3,
-    altText: 'Miss Zainab Mai',
-    title: 'Miss Zainab Mai',
-    designation: 'Officer',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
+  // {
+  //   id: 2,
+  //   imgSrc: Member2,
+  //   altText: 'Hajiya Maimuna Ibrahim',
+  //   title: 'Hajiya Maimuna Ibrahim',
+  //   designation: 'Managemnt Team',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 3,
+  //   imgSrc: Member3,
+  //   altText: 'Miss Zainab Mai',
+  //   title: 'Miss Zainab Mai',
+  //   designation: 'Officer',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
   {
     id: 4,
-    imgSrc: Member4,
+    imgSrc: Kabirutafida,
     altText: 'Kabiru Tafida',
     title: 'Kabiru Tafida',
     designation: 'Officer',
@@ -123,7 +181,7 @@ const data = [
   },
   {
     id: 5,
-    imgSrc: Member5,
+    imgSrc: Hauwahayat,
     altText: 'Hauwa Hayat',
     title: 'Miss Hauwa Hayat',
     designation: 'Officer',
@@ -148,36 +206,36 @@ const data = [
       },
     ],
   },
-  {
-    id: 6,
-    imgSrc: Member1,
-    altText: 'Faruk Kinafa',
-    title: 'Faruk Kinafa',
-    designation: 'Officer',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
+  // {
+  //   id: 6,
+  //   imgSrc: Member1,
+  //   altText: 'Faruk Kinafa',
+  //   title: 'Faruk Kinafa',
+  //   designation: 'Officer',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
   {
     id: 7,
-    imgSrc: Member6,
+    imgSrc: Salimtafida,
     altText: 'Salim Tafida',
     title: 'Salim Tafida',
     designation: 'Client Support Staff',
@@ -204,7 +262,7 @@ const data = [
   },
   {
     id: 8,
-    imgSrc: Member1,
+    imgSrc: Murnajohn,
     altText: 'Miss Murna John',
     title: 'Miss Murna John',
     designation: 'Client Support Staff',
@@ -231,9 +289,9 @@ const data = [
   },
   {
     id: 9,
-    imgSrc: Member2,
-    altText: 'James Gabriel  Dauji',
-    title: 'Mr. James Gabriel  Dauji',
+    imgSrc: Murnajohn,
+    altText: 'Murna John',
+    title: 'Miss. Murna John',
     designation: 'Client Support Staff',
     socialProfile: [
       {
@@ -256,36 +314,36 @@ const data = [
       },
     ],
   },
-  {
-    id: 10,
-    imgSrc: Member3,
-    altText: 'Cyrus Goni',
-    title: 'Mr. Cyrus Goni',
-    designation: 'Client Support Staff',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
+  // {
+  //   id: 10,
+  //   imgSrc: Member3,
+  //   altText: 'Cyrus Goni',
+  //   title: 'Mr. Cyrus Goni',
+  //   designation: 'Client Support Staff',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
   {
     id: 11,
-    imgSrc: Member4,
+    imgSrc: AminaIbrahim,
     altText: 'Hajiya Amina Ibrahim',
     title: 'Hajiya Amina Ibrahim',
     designation: 'Client Support Staff',
@@ -310,141 +368,141 @@ const data = [
       },
     ],
   },
-  {
-    id: 12,
-    imgSrc: Member5,
-    altText: 'Aminu Lawal Abdulkarim',
-    title: 'Mal. Aminu Lawal Abdulkarim',
-    designation: 'Client Support Staff',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 13,
-    imgSrc: Member3,
-    altText: 'Sadiq Usman Mamuda',
-    title: 'Sadiq Usman Mamuda',
-    designation: 'Dev Team',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 14,
-    imgSrc: Member6,
-    altText: 'Jamilu Jibrin',
-    title: 'Jamilu Jibrin',
-    designation: 'Dev Team',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 15,
-    imgSrc: Member5,
-    altText: 'Abdulhafiz Abdullahi',
-    title: 'Abdulhafiz Abdullahi',
-    designation: 'Dev Team',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
-  {
-    id: 16,
-    imgSrc: Member1,
-    altText: "Ahmad Salla'u Gude",
-    title: "Ahmad Salla'u Jr.",
-    designation: 'Dev Team',
-    socialProfile: [
-      {
-        id: 1,
-        name: 'facebook',
-        path: '#',
-        icon: <FaFacebookF />,
-      },
-      {
-        id: 2,
-        name: 'twitter',
-        path: '#',
-        icon: <FaTwitter />,
-      },
-      {
-        id: 3,
-        name: 'instagram',
-        path: '#',
-        icon: <FaInstagram />,
-      },
-    ],
-  },
+  // {
+  //   id: 12,
+  //   imgSrc: Member5,
+  //   altText: 'Aminu Lawal Abdulkarim',
+  //   title: 'Mal. Aminu Lawal Abdulkarim',
+  //   designation: 'Client Support Staff',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 13,
+  //   imgSrc: Member3,
+  //   altText: 'Sadiq Usman Mamuda',
+  //   title: 'Sadiq Usman Mamuda',
+  //   designation: 'Dev Team',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 14,
+  //   imgSrc: Member6,
+  //   altText: 'Jamilu Jibrin',
+  //   title: 'Jamilu Jibrin',
+  //   designation: 'Dev Team',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 15,
+  //   imgSrc: Member5,
+  //   altText: 'Abdulhafiz Abdullahi',
+  //   title: 'Abdulhafiz Abdullahi',
+  //   designation: 'Dev Team',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 16,
+  //   imgSrc: Member1,
+  //   altText: "Ahmad Salla'u Gude",
+  //   title: "Ahmad Salla'u Jr.",
+  //   designation: 'Dev Team',
+  //   socialProfile: [
+  //     {
+  //       id: 1,
+  //       name: 'facebook',
+  //       path: '#',
+  //       icon: <FaFacebookF />,
+  //     },
+  //     {
+  //       id: 2,
+  //       name: 'twitter',
+  //       path: '#',
+  //       icon: <FaTwitter />,
+  //     },
+  //     {
+  //       id: 3,
+  //       name: 'instagram',
+  //       path: '#',
+  //       icon: <FaInstagram />,
+  //     },
+  //   ],
+  // },
 ];
 
 const NgsoftStaff = () => {
